25f6ad2691e09cadffc88513bb1ec008be5d9be5
SimonMelo <melobielcruz@gmail.com>
2024-07-26 11:43:35 -0300

    feat: add interceptors at api

--- src/config/api.js
+++ src/config/api.js
@@ -1,7 +1,25 @@
 const axios = require('axios');
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000', // URL do json-server
+    baseURL: 'http://localhost:3000',
 });
+
+api.interceptors.request.use(() => {
+    config => {
+        return config
+    },
+    error => {
+        return Promise.reject(error)
+    }
+})
+
+api.interceptors.response.use(() => {
+    response => {
+        return response
+    },
+    error => {
+        return Promise.reject(error)
+    }
+})
 
 module.exports = { api };
